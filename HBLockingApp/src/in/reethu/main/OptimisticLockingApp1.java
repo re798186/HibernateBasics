2584f2192aee69078c52ff016fcb5090a584c8ca
re798186 <reethu.thapasya@gmail.com>
2023-03-01 16:32:21 +0400

    Hibernate Locking

---
+++ HBLockingApp/src/in/reethu/main/OptimisticLockingApp1.java
@@ -0,0 +1,45 @@
+package in.reethu.main;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import in.reethu.model.InsurancePolicy;
+import in.reethu.util.HibernateUtil;
+
+public class OptimisticLockingApp1 {
+
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSession();
+		Transaction transaction = null;
+		boolean flag = false;
+
+		try {
+
+			transaction = session.beginTransaction();
+			InsurancePolicy policy = session.get(InsurancePolicy.class, 1L);
+			System.out.println(policy);
+			
+			Thread.sleep(30000);//30seconds(run LockingAppTest2.java now)
+
+			// modify some data with policy object
+			policy.setTenure(12);
+			flag = true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("Object updated...");
+			} else {
+				transaction.rollback();
+				System.out.println("Object not updated...");
+			}
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+
+	}
+
+}
---
+++ HBLockingApp/src/in/reethu/main/OptimisticLockingApp2.java
@@ -0,0 +1,44 @@
+package in.reethu.main;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import in.reethu.model.InsurancePolicy;
+import in.reethu.util.HibernateUtil;
+
+public class OptimisticLockingApp2 {
+
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSession();
+		Transaction transaction = null;
+		boolean flag = false;
+
+		try {
+
+			transaction = session.beginTransaction();
+			InsurancePolicy policy = session.get(InsurancePolicy.class, 1L);
+			System.out.println(policy);
+			
+
+			// modify some data with policy object
+			policy.setTenure(1);
+			flag = true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("Object updated...");
+			} else {
+				transaction.rollback();
+				System.out.println("Object not updated...");
+			}
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+
+	}
+
+}
